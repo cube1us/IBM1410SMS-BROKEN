6d35ef5c7468d98c7b885d040def2c38c6b0e028
Jay Jaeger <cube1us@gmail.com>
2020-02-17 19:37:42 +0000

    Added copyright information
    git-svn-id: svn://localhost/active/IBM1410SMS/trunk@1595 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- MySQLFramework/Properties/AssemblyInfo.cs
+++ MySQLFramework/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MySQLFramework")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018, 2019, 2020 Jay R. Jaeger")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
